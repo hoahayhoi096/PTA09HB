bb8606aae853629e6afa505af31f878a12321098
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-12 07:43:22 +0700

    .

---
+++ Bai01/BT01.py
@@ -0,0 +1,17 @@
+# Biến là gì? Cách sử dụng biến? 
+
+# Nhập/ xuất giá trị ? 
+
+# Câu lệnh rẽ nhánh?
+
+# Vòng lặp? Cách sử dụng vòng lặp?  
+
+# Danh Sách? Cách sử dụng danh sách?
+
+# Xâu? Cách sử dụng xâu? 
+
+# Hàm? Cách sử dụng hàm? 
+
+# Làm bài tập 
+
+
