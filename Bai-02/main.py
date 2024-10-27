4f1704875bc24540e9c081e3ca221663c9a1a28e
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-27 11:08:15 +0700

    Bai2: Class xe

--- Bai-02/main.py
+++ Bai-02/main.py
@@ -20,9 +20,32 @@ dsHocSinh = [hocSinh1, hocSinh2]
 # print(hocSinh1.tuoi)
 # print(hocSinh1.lopHoc)
 
-for i in range(0, len(dsHocSinh)):
-    print(dsHocSinh[i].ten)
-    print(dsHocSinh[i].tuoi)
-    print(dsHocSinh[i].lopHoc)
-    print("Hết một học sinh")
+# for i in range(0, len(dsHocSinh)):
+#     print(dsHocSinh[i].ten)
+#     print(dsHocSinh[i].tuoi)
+#     print(dsHocSinh[i].lopHoc)
+#     print("Hết một học sinh")
 
+
+class Xe:
+    hangXe = ""
+    soChoNgoi = 0 
+    mauSac = ""
+
+xe1 = Xe()
+xe1.hangXe = "Toyota"
+xe1.soChoNgoi = 4 
+xe1.mauSac = "Đỏ"
+
+xe2 = Xe()
+xe2.hangXe = "Lambogini"
+xe2.soChoNgoi = 2 
+xe2.mauSac = "Vàng"
+
+dsXe = [xe1, xe2]
+
+for i in range(0, len(dsXe)):
+    print(dsXe[i].hangXe)
+    print(dsXe[i].soChoNgoi)
+    print(dsXe[i].mauSac)
+    print("")
