a38626637201fbaaeae15c8e4ef46a1c7e798edf
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-10 11:25:48 +0700

    Buoi 04: class PhuongTien

--- Bai-04/main.py
+++ Bai-04/main.py
@@ -1,9 +1,16 @@
 from classes.DongVatCls import DongVat
+from classes.PhuongTienCls import XeMay, Oto
 
-dongVat1 = DongVat("Sư tử", 10, "Ăn thit", "Châu phi")
+# dongVat1 = DongVat("Sư tử", 10, "Ăn thit", "Châu phi")
 
-dongVat1.inThongTin()
+# dongVat1.inThongTin()
 
-dongVat1.capNhatMoiTrongSong("Trong nhà")
+# dongVat1.capNhatMoiTrongSong("Trong nhà")
 
-dongVat1.inThongTin()
+# dongVat1.inThongTin()
+xeMay = XeMay("Xe tay ga", "12ABC", "Kawasaki", "Đỏ")
+
+oTo = Oto("Bốn chỗ", "12ABD", "Honda", "Trắng")
+
+xeMay.display_info()
+oTo.display_info()
